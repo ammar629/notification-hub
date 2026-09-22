b43c8349c447e738c80c1898a7bdde086259e1e8
ammar629 <ammarahmed0003@gmail.com>
2026-09-22 15:20:35 +0500

    chore(backend): link authRouter in app.ts

--- notification-hub-backend/src/app.ts
+++ notification-hub-backend/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 
+// Routes
+import authRouter from "@/routes/auth";
+
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.get("/health", (req: Request, res: Response) => {
   res.json({ status: "ok" }); 
