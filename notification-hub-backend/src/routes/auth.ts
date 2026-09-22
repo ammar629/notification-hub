fd4cf2b4ff301117027db26059a8abb26a2f6a23
ammar629 <ammarahmed0003@gmail.com>
2026-09-22 15:18:28 +0500

    feat(backend): implement auth router

---
+++ notification-hub-backend/src/routes/auth.ts
@@ -0,0 +1,28 @@
+import {Router, Request, Response} from "express";
+import {db} from "@/db";
+import {users, NewUser} from "@/db/schema";
+import {eq} from "drizzle-orm";
+
+const router: Router = Router();
+
+// POST /auth/signup
+router.post("/signup", async (req: Request, res: Response) => {
+    try{
+        const {email, password} = req.body;
+        const newUser: NewUser = {
+             email,
+             password, // TODO: Hash password with bcrypt later
+             isAdmin: false,
+            };
+
+        const result = await db.insert(users).values(newUser).returning();
+
+        res.status(201).json({ message: "User created successfully" });
+    }
+    catch(error){
+        res.status(400).json({error: "Signup Failed"});
+    }
+});
+
+
+export default router;
